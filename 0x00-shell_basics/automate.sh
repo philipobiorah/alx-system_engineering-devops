59e8bcdda76bd60960547561d219ab1a87a3d5e9
philipobiorah <philip.obiorah@outlook.com>
2022-03-02 16:49:55 -0800

    Auto updates 9-14

--- 0x00-shell_basics/automate.sh
+++ 0x00-shell_basics/automate.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo -e '#!/bin/bash \nls -lna ' > 5-listfilesdigitonly && chmod u+x 5-listfilesdigitonly
-echo -e '#!/bin/bash \nmkdir /tmp/my_first_directory' > 6-firstdirectory && chmod u+x 6-firstdirectory
+echo -e '#!/bin/bash \nmv /tmp/betty /tmp/my_first_directory' > 7-movethatfile && chmod u+x 7-movethatfile
+echo -e '#!/bin/bash \nrm /tmp/my_first_directory/betty' > 8-firstdelete && chmod u+x 8-firstdelete
+echo -e '#!/bin/bash \nrmdir /tmp/my_first_directory' > 9-firstdirdeletion && chmod u+x 9-firstdirdeletion
+echo -e '#!/bin/bash \ncd -' > 10-back && chmod u+x 10-back
+echo -e '#!/bin/bash \nls -al . .. /boot' > 11-lists && chmod u+x 11-lists
+echo -e '#!/bin/bash \nfile /tmp/iamafile' > 12-file_type && chmod u+x 12-file_type
+echo -e '#!/bin/bash \nln -s /bin/ls __ls__' > 13-symbolic_link && chmod u+x 13-symbolic_link
+echo -e '#!/bin/bash \ncp -rua *.html ../' > 14-copy_html && chmod u+x 14-copy_html
