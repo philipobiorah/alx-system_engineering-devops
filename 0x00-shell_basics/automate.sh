0bdc08e2cd87d64b1fb413fe0802b9a3ffefc0d9
philipobiorah <philip.obiorah@outlook.com>
2022-03-02 16:24:43 -0800

    Add list files digit and first dir

---
+++ 0x00-shell_basics/automate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -e '#!/bin/bash \nls -lna ' > 5-listfilesdigitonly && chmod u+x 5-listfilesdigitonly
+echo -e '#!/bin/bash \nmkdir /tmp/my_first_directory' > 6-firstdirectory && chmod u+x 6-firstdirectory
