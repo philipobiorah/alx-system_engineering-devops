96034c9db3113fec64d9a941a5eabb87d3fec746
philipobiorah <philip.obiorah@outlook.com>
2022-03-02 17:17:00 -0800

    lets move clean emacs tree commas

--- 0x00-shell_basics/automate.sh
+++ 0x00-shell_basics/automate.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo -e '#!/bin/bash \nmv /tmp/betty /tmp/my_first_directory' > 7-movethatfile && chmod u+x 7-movethatfile
-echo -e '#!/bin/bash \nrm /tmp/my_first_directory/betty' > 8-firstdelete && chmod u+x 8-firstdelete
-echo -e '#!/bin/bash \nrmdir /tmp/my_first_directory' > 9-firstdirdeletion && chmod u+x 9-firstdirdeletion
-echo -e '#!/bin/bash \ncd -' > 10-back && chmod u+x 10-back
-echo -e '#!/bin/bash \nls -al . .. /boot' > 11-lists && chmod u+x 11-lists
-echo -e '#!/bin/bash \nfile /tmp/iamafile' > 12-file_type && chmod u+x 12-file_type
-echo -e '#!/bin/bash \nln -s /bin/ls __ls__' > 13-symbolic_link && chmod u+x 13-symbolic_link
-echo -e '#!/bin/bash \ncp -rua *.html ../' > 14-copy_html && chmod u+x 14-copy_html
+echo -e '#!/bin/bash \nmv [[:upper:]]* /tmp/u' > 100-lets_move && chmod u+x 100-lets_move
+echo -e '#!/bin/bash \nrm *~' > 101-clean_emacs && chmod u+x 101-clean_emacs
+echo -e '#!/bin/bash \nmkdir -p welcome/to/school' > 102-tree && chmod u+x 102-tree
+echo -e '#!/bin/bash \nls -map' > 103-commas && chmod u+x 103-commas
+
